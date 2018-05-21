a6cdd0e755cf621674d0872a3e1485bc197ff0d3
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-21 11:19:17 -0400

    Add recursion instead of iteration

--- solution.rb
+++ solution.rb
@@ -1,19 +1,31 @@
-require 'pry'
+# require 'pry'
 def palindrome?(word)
-  reversed_word = ""
-
-  word.split("").each do |letter|
-    #binding.pry
-
-    reversed_word = letter + reversed_word
+  if word.length == 0 || word.length == 1
+    puts true
+  elsif word[0] == word[-1]
+    palindrome?(word[1..-2])
+  else puts false
   end
-puts reversed_word
-puts word
-puts word == reversed_word
 end
+
+# def palindrome?(word)
+#   reversed_word = ""
+#
+#   word.split("").each do |letter|
+#     #binding.pry
+#
+#     reversed_word = letter + reversed_word
+#   end
+# puts reversed_word
+# puts word
+# puts word == reversed_word
+# end
 
 
 # def palindrome?(word)
 # word = word.reverse
 # end
-palindrome?("alberto")
+palindrome?("noon")
+palindrome?("five")
+palindrome?("radar")
+palindrome?("learnlovecode")
