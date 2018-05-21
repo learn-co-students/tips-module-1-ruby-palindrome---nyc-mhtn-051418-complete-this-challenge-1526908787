c29b591789fc7838f8dccb9b66f45cc9d35b76f7
squamuglia <maxsmouha@gmail.com>
2018-05-21 10:25:42 -0400

    Done.

--- palindrome.rb
+++ palindrome.rb
@@ -1,3 +1,20 @@
 def palindrome?(string)
- string ==  string.reverse!
+ string == string.reverse!
+end
+
+
+def reverse_string(string)
+  ctr = (string.length - 1)
+  a = []
+  while ctr >= 0
+    a << string[ctr]
+    ctr -= 1
+  end
+  b = a.join("")
+  b
+end
+
+
+def palindrome?(string)
+ reverse_string(string) == string
 end
