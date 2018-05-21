144d1e9df308eb386e9427bac80b60e6ffdb97b8
emptycan <richardtrapanese@gmail.com>
2018-05-21 10:33:11 -0400

    done

---
+++ palindrome.rb
@@ -0,0 +1,19 @@
+
+require 'pry'
+def palindrome?(string)
+  array []
+  array << string
+  #iterate over the array with the argument to test if it is a
+  # palindrome
+  x.length
+  array.each do |v|
+    binding.pry
+    #check and compare each character in array, if first and last are =
+     v.each do |x|
+      if v[0] == x
+        puts "This #{v} is a palindrome."
+      else
+      puts "Value of array is #{v} is not a palindrome."
+      end
+  end
+end
